6adeeda37604721d1ad248b56afd496b93f11bca
m1stadev <adamhamdi31@gmail.com>
2022-04-04 18:08:33 -0500

    [main] Empty out CLI for now

--- pyimg4/__main__.py
+++ pyimg4/__main__.py
@@ -1,13 +1,7 @@
-import argparse
 import pyimg4
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(
-        usage='pyimg4 [options]',
-    )
-    args = parser.parse_args()
-
     print(f'pyimg4 {pyimg4.__version__}')
 
 

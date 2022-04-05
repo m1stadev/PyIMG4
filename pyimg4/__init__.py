a593dea1d613f151a8e0a2321bd47ca31e205417
m1stadev <adamhamdi31@gmail.com>
2022-04-04 20:36:07 -0500

    Update package imports

--- pyimg4/__init__.py
+++ pyimg4/__init__.py
@@ -1,5 +1,5 @@
 from importlib.metadata import version
-from .parser import IM4P, IM4M, IMG4
+from .parser import IM4P, IM4PData, IM4M, IMG4
 from .types import *
 
 __version__ = version(__package__)

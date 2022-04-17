9887adc0fdc9f86a9d8f17dee85b44ddd1bd4029
m1stadev <adamhamdi31@gmail.com>
2022-04-17 10:53:56 -0500

    [tests] IM4M: More in-depth checking

--- tests/test_im4m.py
+++ tests/test_im4m.py
@@ -9,5 +9,9 @@ def test_im4m(IM4M: bytes) -> None:
         == '0123456789012345678901234567890123456789012345678901234567890123'
     )
 
+    assert im4m.chip_id == 0x8015
     assert im4m.ecid == 0x0123456789012
     assert im4m.sepnonce == '0123456789012345678901234567890123456789'
+
+    assert len(im4m.properties) == 11
+    assert len(im4m.images) == 35

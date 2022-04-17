a472e66ba805ef7f9945fa8231ef044487a67a36
m1stadev <adamhamdi31@gmail.com>
2022-04-17 13:08:20 -0500

    [tests] Add an IMG4 test

--- tests/test_img4.py
+++ tests/test_img4.py
@@ -8,3 +8,14 @@ def test_create(IM4M: bytes, IM4P: bytes, IMG4: bytes) -> None:
     img4 = im4m + im4p
 
     assert img4.output() == IMG4
+
+
+def test_create_with_im4r(IM4M: bytes, IM4P: bytes, IM4R: bytes) -> None:
+    im4m = pyimg4.IM4M(IM4M)
+    im4p = pyimg4.IM4P(IM4P)
+
+    img4 = im4m + im4p
+
+    img4.im4r = IM4R
+
+    img4.output()

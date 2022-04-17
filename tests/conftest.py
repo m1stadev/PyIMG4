958673af558319f58057377c539e2e20e613c2b7
m1stadev <adamhamdi31@gmail.com>
2022-04-17 02:35:42 -0500

    [tests] Add fixture for getting encrypted LZSS IM4P

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,20 @@ from remotezip import RemoteZip
 
 import pytest
 
-LZSS_IPSW = 'https://updates.cdn-apple.com/2021FallFCS/fullrestores/002-03194/8EC63AF9-19BE-4829-B389-27AECB41DD6A/iPhone_5.5_15.0_19A346_Restore.ipsw'
+LZSS_DEC_IPSW = 'https://updates.cdn-apple.com/2021FallFCS/fullrestores/002-03194/8EC63AF9-19BE-4829-B389-27AECB41DD6A/iPhone_5.5_15.0_19A346_Restore.ipsw'
+LZSS_ENC_IPSW = 'http://appldnld.apple.com/iOS9.3.5/031-73130-20160825-6A2C2FD4-6711-11E6-B3F4-173834D2D062/iPhone8,2_9.3.5_13G36_Restore.ipsw'
 LZFSE_IPSW = 'https://updates.cdn-apple.com/2021FallFCS/fullrestores/002-02910/AF984499-D03A-43E7-9472-6D16BA756E5E/iPhone10,3,iPhone10,6_15.0_19A346_Restore.ipsw'
 
 
 @pytest.fixture(name='dec_lzss', scope='session')
 def fetch_dec_lzss_im4p() -> bytes:
-    with RemoteZip(LZSS_IPSW) as ipsw:
+    with RemoteZip(LZSS_DEC_IPSW) as ipsw:
+        return ipsw.read('kernelcache.release.n66')
+
+
+@pytest.fixture(name='enc_lzss', scope='session')
+def fetch_enc_lzss_im4p() -> bytes:
+    with RemoteZip(LZSS_ENC_IPSW) as ipsw:
         return ipsw.read('kernelcache.release.n66')
 
 

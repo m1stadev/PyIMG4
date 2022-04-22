00a50e68fe3bcc9beb8b44944176cc1ae7e0798d
m1stadev <adamhamdi31@gmail.com>
2022-04-21 23:21:01 -0500

    Update how package version is found

--- pyimg4/__init__.py
+++ pyimg4/__init__.py
@@ -1,6 +1,10 @@
 from .errors import *
 from .parser import *
 from .types import *
-from importlib.metadata import version
+
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version
 
 __version__ = version(__package__)

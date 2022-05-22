5d8569e2f49e14b5c3d8fb5f72991f93b6e497a8
m1stadev <adamhamdi31@gmail.com>
2022-05-22 01:41:02 -0500

    [CLI] Update - Update type-hinting - Update import formatting - Add version option - Print more info in `pyimg4 im4p info` - Update `pyimg4 im4p extract`   - Allow extracting payload data without decompressing - Better error handling for commands

--- pyimg4/__main__.py
+++ pyimg4/__main__.py
@@ -1,76 +1,194 @@
-import typing
+from pathlib import Path
+from typing import BinaryIO, Optional
 
 import click
 
 import pyimg4
-from pyimg4 import Keybag, Compression
+from pyimg4 import Compression, Keybag
 
 
 @click.group()
+@click.version_option(message=f'PyIMG4 {pyimg4.__version__}')
 def cli():
-    """Swiss army knife to Apple's Image4 format"""
-    pass
+    '''A Python CLI tool for parsing Apple's Image4 format.'''
 
-
-@cli.command()
-def version() -> None:
-    """Get current package version"""
-    print(f'pyimg4 {pyimg4.__version__}')
+    click.echo(f'PyIMG4 {pyimg4.__version__}')
 
 
 @cli.group()
 def im4p() -> None:
-    """IM4P image options"""
+    '''Image4 payload commands'''
+
     pass
 
 
 @im4p.command('info')
-@click.argument('file', type=click.File('rb'))
-def img4p_info(file) -> None:
-    """Print available information on given im4p image"""
-    im4p = pyimg4.IM4P(file.read())
-    print(f'fourcc: {im4p.fourcc}')
-    print(f'description: {im4p.description}')
-    print(f'payload length: {len(im4p.payload)} Bytes')
-    print(f'payload encrypted: {im4p.payload.encrypted}')
-    print(f'payload compression: {im4p.payload.compression.name}')
+@click.argument('im4p', type=click.File('rb'))
+def im4p_info(im4p) -> None:
+    '''Print available information on an Image4 payload'''
 
+    click.echo('Reading Image4 payload file...')
+    im4p = pyimg4.IM4P(im4p.read())
 
-@im4p.command('payload')
-@click.argument('file', type=click.File('rb'))
-@click.argument('out', type=click.File('wb'))
-@click.option('-i', '--iv', help='The IV used to encrypt the payload')
-@click.option('-k', '--key', help='The key used to encrypt the payload')
-def img4_payload(file, out, iv, key) -> None:
-    """Extract the payload within the IM4P image"""
-    im4p = pyimg4.IM4P(file.read())
-    im4p.payload.decrypt(Keybag(key=key, iv=iv))
-    if im4p.payload.compression > Compression.NONE:
+    click.echo('  Image4 payload info:')
+    click.echo(f'    Image4 payload FourCC: {im4p.fourcc}')
+    click.echo(f'    Image4 payload description: {im4p.description}')
+    click.echo(f'    Image4 payload data size: {round(len(im4p.payload) / 1000)}KB')
+
+    if (
+        im4p.payload.encrypted == False
+        and im4p.payload.compression != pyimg4.Compression.NONE
+    ):
+        click.echo(
+            f'    Image4 payload data compression type: {im4p.payload.compression.name}'
+        )
+
         im4p.payload.decompress()
-    out.write(im4p.payload.output())
+        click.echo(
+            f'    Image4 payload data size (uncompressed): {round(len(im4p.payload) / 1000)}KB'
+        )
+
+    if im4p.payload.encrypted:
+        click.echo(f'    Image4 payload data encrypted: {im4p.payload.encrypted}\n')
+        for kb in im4p.payload.keybags:
+            click.echo('    Keybag:')
+            click.echo(f'      Type: {kb.type.name}')
+            click.echo(f'      IV: {kb.iv.hex()}')
+            click.echo(f'      Key: {kb.key.hex()}')
+
+
+@im4p.command('extract')
+@click.option(
+    '-i',
+    '--input',
+    'input_',
+    type=click.File('rb'),
+    required=True,
+    help='Input Image4 payload file',
+)
+@click.option(
+    '-o',
+    '--output',
+    type=click.Path(dir_okay=False, exists=False, path_type=Path, writable=True),
+    required=True,
+    help='File to output Image4 payload data to',
+)
+@click.option(
+    '--no-decompress',
+    'decompress',
+    default=True,
+    is_flag=True,
+    help="Don't decompress the Image4 payload data",
+)
+@click.option('--iv', help='The IV used to encrypt the Image4 payload data')
+@click.option('--key', help='The key used to encrypt the Image4 payload data')
+def im4p_extract(
+    input_: Path,
+    output: Path,
+    decompress: bool,
+    iv: Optional[str],
+    key: Optional[str],
+) -> None:
+    '''Extract data from an Image4 payload'''
+
+    click.echo('Reading Image4 payload file...')
+    im4p = pyimg4.IM4P(input_.read())
+
+    if im4p.payload.encrypted == True:
+        if iv is None and key is None:
+            click.echo('[NOTE] Image4 payload data is encrypted')
+        else:
+            click.echo('[NOTE] Image4 payload data is encrypted, decrypting...')
+
+        if (iv is None and key is not None) or (key is None and iv is not None):
+            raise click.BadParameter('You must specify both the IV and the key')
+
+        else:
+            if len(key) != 64:
+                raise click.BadParameter('Key must be 64 characters long')
+
+            if len(iv) != 32:
+                raise click.BadParameter('IV must be 32 characters long')
+
+            try:
+                key = bytes.fromhex(key)
+            except TypeError:
+                raise click.BadParameter('Key must be a hex string')
+
+            try:
+                iv = bytes.fromhex(iv)
+            except TypeError:
+                raise click.BadParameter('IV must be a hex string')
+
+            im4p.payload.decrypt(Keybag(key=key, iv=iv))
+
+    if im4p.payload.compression not in (Compression.NONE, Compression.UNKNOWN):
+        if decompress == True:
+            click.echo(
+                f'[NOTE] Image4 payload data is {im4p.payload.compression.name} compressed, decompressing...'
+            )
+
+            im4p.payload.decompress()
+        else:
+            click.echo(
+                f'[NOTE] Image4 payload data is {im4p.payload.compression.name} compressed, skipping decompression'
+            )
+
+    with output.open('wb') as f:
+        f.write(im4p.payload.output())
+
+    click.echo(f'Extracted Image4 payload data to: {output}')
 
 
 @cli.group()
 def img4() -> None:
-    """IMG4 image options"""
+    '''Image4 file commands'''
+
     pass
 
 
-@im4p.command('extract')
-@click.argument('file', type=click.File('rb'))
-@click.option('--im4p', type=click.File('wb'), help='IM4P output file')
-@click.option('--im4m', type=click.File('wb'), help='IM4M output file')
-@click.option('--im4r', type=click.File('wb'), help='IM4R output file')
+@img4.command('extract')
+@click.option(
+    '-i', '--img4', type=click.File('rb'), help='Input Image4 file', required=True
+)
+@click.option(
+    '-p', '--im4p', type=click.File('wb'), help='File to output Image4 payload to'
+)
+@click.option(
+    '-m', '--im4m', type=click.File('wb'), help='File to output Image4 manifest to'
+)
+@click.option(
+    '-r', '--im4r', type=click.File('wb'), help='File to output Image4 restore info to'
+)
 def img4_extract(
-    file: typing.IO, im4p: typing.IO, im4m: typing.IO, im4r: typing.IO
+    img4: Optional[BinaryIO],
+    im4p: Optional[BinaryIO],
+    im4m: Optional[BinaryIO],
+    im4r: Optional[BinaryIO],
 ) -> None:
-    """Extract IM4P, IM4R & IM4M images"""
-    img4 = pyimg4.IMG4(file.read())
-    if im4p:
+    '''Extract Image4 manifest/payload/restore info from an Image4 file'''
+
+    img4 = pyimg4.IMG4(img4.read())
+
+    if not any(i is not None for i in (im4p, im4m, im4r)):
+        raise click.BadParameter('You must specify at least one output file')
+
+    if im4p is not None:
+        if img4.im4p is None:
+            raise click.BadParameter('Image4 payload not found in Image4 file')
+
         im4p.write(img4.im4p.output())
-    if im4m:
+
+    if im4m is not None:
+        if img4.im4m is None:
+            raise click.BadParameter('Image4 manifest not found in Image4 file')
+
         im4m.write(img4.im4m.output())
-    if im4r:
+
+    if im4r is not None:
+        if img4.im4r is None:
+            raise click.BadParameter('Image4 restore info not found in Image4 file')
+
         im4r.write(img4.im4r.output())
 
 

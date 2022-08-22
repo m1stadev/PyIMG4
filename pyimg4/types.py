d077f707e4ec35625e8b7734b5af78f522585c36
m1stadev <adamhamdi31@gmail.com>
2022-08-22 12:28:50 -0500

    [types] Add initial support for encrypted LZFSE-compressed payloads

--- pyimg4/types.py
+++ pyimg4/types.py
@@ -7,11 +7,13 @@ class Compression(IntEnum):
     NONE = 0x0
     LZSS = 0x1
     LZFSE = 0x2
+    LZFSE_ENCRYPTED = 0x3
 
 
 class Payload(NamedTuple):
     data: bytes
     keybags: Optional[bytes]
+    lzfse_length: Optional[bytes]
 
 
 class KeybagType(IntEnum):

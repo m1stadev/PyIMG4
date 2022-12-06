57e24effc79142825411bf740f26aae267ed9a5a
m1stadev <adamhamdi31@gmail.com>
2022-12-06 16:02:34 -0600

    [tests] test_im4p: Refactor, add PayloadProperties test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from random import getrandbits
 
 import pytest
 from remotezip import RemoteZip
@@ -7,33 +6,40 @@ from remotezip import RemoteZip
 LZSS_DEC_IPSW = 'https://updates.cdn-apple.com/2021FallFCS/fullrestores/002-03194/8EC63AF9-19BE-4829-B389-27AECB41DD6A/iPhone_5.5_15.0_19A346_Restore.ipsw'
 LZSS_ENC_IPSW = 'http://appldnld.apple.com/iOS9.3.5/031-73130-20160825-6A2C2FD4-6711-11E6-B3F4-173834D2D062/iPhone8,2_9.3.5_13G36_Restore.ipsw'
 LZFSE_IPSW = 'https://updates.cdn-apple.com/2021FallFCS/fullrestores/002-02910/AF984499-D03A-43E7-9472-6D16BA756E5E/iPhone10,3,iPhone10,6_15.0_19A346_Restore.ipsw'
+PAYP_IPSW = 'https://updates.cdn-apple.com/2022FallFCS/fullrestores/032-11449/3995EAD9-8F0E-491E-BEC8-C97B6B1845C7/iPhone15,3_16.1.2_20B110_Restore.ipsw'
 
 
-@pytest.fixture(name='dec_lzss', scope='session')
+@pytest.fixture(name='DEC_LZSS_IM4P', scope='session')
 def fetch_dec_lzss_im4p() -> bytes:
     with RemoteZip(LZSS_DEC_IPSW) as ipsw:
         return ipsw.read('kernelcache.release.n66')
 
 
-@pytest.fixture(name='dec_lzfse', scope='session')
-def fetch_dec_lzfse_im4p() -> bytes:
-    with RemoteZip(LZFSE_IPSW) as ipsw:
-        return ipsw.read('kernelcache.release.iphone10b')
-
-
-@pytest.fixture(name='enc_lzss', scope='session')
+@pytest.fixture(name='ENC_LZSS_IM4P', scope='session')
 def fetch_enc_lzss_im4p() -> bytes:
     with RemoteZip(LZSS_ENC_IPSW) as ipsw:
         return ipsw.read('kernelcache.release.n66')
 
 
-@pytest.fixture(name='enc_lzfse', scope='session')
+@pytest.fixture(name='DEC_LZFSE_IM4P', scope='session')
+def fetch_dec_lzfse_im4p() -> bytes:
+    with RemoteZip(LZFSE_IPSW) as ipsw:
+        return ipsw.read('kernelcache.release.iphone10b')
+
+
+@pytest.fixture(name='ENC_LZFSE_IM4P', scope='session')
 def fetch_enc_lzfse_im4p() -> bytes:
     with RemoteZip(LZFSE_IPSW) as ipsw:
         return ipsw.read('Firmware/dfu/iBSS.d22.RELEASE.im4p')
 
 
-@pytest.fixture(name='test_data', scope='session')
+@pytest.fixture(name='PAYP_IM4P', scope='session')
+def fetch_payp_im4p() -> bytes:
+    with RemoteZip(PAYP_IPSW) as ipsw:
+        return ipsw.read('Firmware/dfu/iBSS.d74.RELEASE.im4p')
+
+
+@pytest.fixture(name='TEST_PAYLOAD', scope='session')
 def fetch_test_payload() -> bytes:
     with (Path(__file__).parent / 'bin' / 'test_payload').open('rb') as f:
         return f.read()

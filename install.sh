319a38b289f211357dc4899e8dd9657a4f1b8ea2
m1stadev <adamhamdi31@gmail.com>
2025-02-26 16:28:49 -0600

    proj: switch to uv

--- install.sh
+++ install.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-# Run checks
+# Sanity check
 if ! which python3 > /dev/null; then
 	echo "[ERROR] Python 3 is not installed."
 	exit 1
 fi
 
-if ! which poetry > /dev/null; then
-	echo "[ERROR] poetry is not installed."
-	exit 1
-fi
-
-if [ -d "dist/" ]; then
-	rm -rf dist/
-fi
-
-# Build package
-poetry build
-
 # Install package
-python3 -m pip install $(ls dist/*.tar.gz)
+python3 -m pip install $(dirname "$0")
